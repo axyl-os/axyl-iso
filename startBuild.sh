941b8b28f00ca700a42e222141dbc07692a758d9
Kungger-git <matthewriel63@gmail.com>
2022-03-31 19:23:23 +0800

    [Update]: Simplified the docker build script.

--- startBuild.sh
+++ startBuild.sh
@@ -12,11 +12,28 @@ _user=`echo ${SUDO_USER:-$(whoami)}`
 _gid=`echo ${SUDO_GID}`
 _group=`cat /etc/group | grep ${_gid} | cut -d: -f1 | head -1`
 _path="/var/lib/docker/volumes/axyl-vol/_data"
+_iso="/home/${_user}/Axyl-Iso"
+
+## Delete existing Axyl-Iso directory from home user
+echo "+---------------------->>"
+echo "[*] Delete existing ${_iso}..."
+if [[ -d ${_iso} ]]; then
+  rm -rf ${_iso}
+fi
+
+## Delete all existing Docker Images
+echo "+---------------------->>"
+echo "[*] Delete all existing Docker Images..."
+yes | docker system prune -a
 
 ## Building the Docker Image
+echo "+---------------------->>"
+echo "[*] Build 'axyl-build' Docker Image..."
 docker build -t axyl-build ./
 
 ## Running the Docker Image
+echo "+---------------------->>"
+echo "[*] Run Docker Image..."
 docker volume create axyl-vol
 
 if [[ ! -d ${_path} ]]; then
@@ -26,7 +43,11 @@ fi
 docker run --privileged --mount source=axyl-vol,target=/axyl-iso/out axyl-build
 
 ## Move ISO data directory to home folder & change ownership to current user
-mv ${_path} /home/${_user}/Axyl-Iso
+echo "+---------------------->>"
+echo "[*] Move '${_path}' directory and rename as '${_iso}'..."
+mv ${_path} ${_iso}
 
 ## Change ownership of ~/Axyl-Iso
-chown -R ${_user}:${_group} /home/${_user}/Axyl-Iso
+echo "+---------------------->>"
+echo "[*] Change '${_iso}' ownership to '${_user}'..."
+chown -R ${_user}:${_group} ${_iso}
