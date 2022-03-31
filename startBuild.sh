5087229cc659133c3d83de92fc25eb810d00041b
Kungger-git <matthewriel63@gmail.com>
2022-03-31 10:06:43 +0800

    [Fix]: Bug Fix for build script. Use `sudo` to execute `startBuild.sh` file.

--- startBuild.sh
+++ startBuild.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
+set -e
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
 
 ## Change ownership variables.
-local _user=`echo ${SUDO_USER:-$(whoami)}`
-local _gid=`echo ${SUDO_GID}`
-local _group=`cat /etc/group | grep ${_gid} | cut -d: -f1 | head -1`
+_user=`echo ${SUDO_USER:-$(whoami)}`
+_gid=`echo ${SUDO_GID}`
+_group=`cat /etc/group | grep ${_gid} | cut -d: -f1 | head -1`
+_path="/var/lib/docker/volumes/axyl-vol/_data"
 
 ## Building the Docker Image
-sudo docker build -t axyl-build ./
+docker build -t axyl-build ./
 
 ## Running the Docker Image
-sudo docker volume create axyl-vol
-sudo docker run --privileged --mount source=axyl-vol,target=/axyl-iso/out axyl-build
+docker volume create axyl-vol
+
+if [[ ! -d ${_path} ]]; then
+  mkdir -p ${_path}
+fi
+
+docker run --privileged --mount source=axyl-vol,target=/axyl-iso/out axyl-build
 
 ## Move ISO data directory to home folder & change ownership to current user
-sudo mv /var/lib/docker/volumes/axyl-vol/_data ~/Axyl-Iso
+mv ${_path} /home/${_user}/Axyl-Iso
 
 ## Change ownership of ~/Axyl-Iso
-chown -R ${_user}:${_group} ~/Axyl-Iso
+chown -R ${_user}:${_group} /home/${_user}/Axyl-Iso
